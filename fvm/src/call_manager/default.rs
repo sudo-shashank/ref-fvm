8b81cd0c4defd9a1db0bbac3b450bdd6d8d48b0c
Steven Allen <steven@stebalien.com>
2023-08-01 07:56:00 -0700

    more cleanups

--- fvm/src/call_manager/default.rs
+++ fvm/src/call_manager/default.rs
@@ -674,7 +674,14 @@ where
         }
 
         // Charge the invocation gas.
-        let t = self.charge_gas(self.price_list().on_method_invocation())?;
+        let (param_size, param_links) = params
+            .as_ref()
+            .map(|p| (p.size(), p.links().len()))
+            .unwrap_or_default();
+        let t = self.charge_gas(
+            self.price_list()
+                .on_method_invocation(param_size, param_links),
+        )?;
 
         // Store the parametrs, and initialize the block registry for the target actor.
         let mut block_registry = BlockRegistry::new();
@@ -841,18 +848,17 @@ where
             // future, we'll charge for internal returns as well (to charge for link tracking).
             // Unfortunately, we have to do this _here_ instead of in the caller as we need to apply
             // the call's gas limit.
-            if let Some(ret_size) = ret
+            if let Some((ret_size, link_count)) = ret
                 .as_ref()
                 .ok()
                 .and_then(|r| r.value.as_ref())
-                .map(|v| v.size())
+                .map(|v| (v.size(), v.links().len()))
             {
-                if let Some(charge) = cm
-                    .price_list()
-                    .on_method_return(cm.call_stack_depth, ret_size)
-                {
-                    let _ = cm.charge_gas(charge);
-                }
+                let _ = cm.charge_gas(cm.price_list().on_method_return(
+                    cm.call_stack_depth,
+                    ret_size,
+                    link_count,
+                ));
             }
 
             // Log the results if tracing is enabled.
